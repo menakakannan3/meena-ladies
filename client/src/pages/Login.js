fb14bcbf6616d503fdf0bd92812a2d4006301fec
Menaka kannan <86248020+menakakannan3@users.noreply.github.com>
2024-12-03 19:01:44 +0530

    Update Login.js

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -17,7 +17,7 @@ const Login = () => {
     setError(null);
 
     try {
-      const response = await fetch("http://localhost:4000/api/login", {
+      const response = await fetch("https://meena-ladies-server.onrender/api/login.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
